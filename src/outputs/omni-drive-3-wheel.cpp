cd9d4592c20908f92fe9f12eda65c128a1e1cac2
Peter Moyer <petersdragon@gmail.com>
2020-11-08 14:50:29 -0600

    Added some comments to omni-drive-3-wheel.cpp

--- src/outputs/omni-drive-3-wheel.cpp
+++ src/outputs/omni-drive-3-wheel.cpp
@@ -9,13 +9,27 @@ namespace godspeed
 
     void OmniDrive3Wheel::update()
     {
-      double x = xDirection.value();
+      // Read in the x coordinate value of
+      // the left joystick on the controller
+      double x = xDirection.value(); 
+      // Read in the y coordinate value of
+      // the left joystick on the controller
       double y = yDirection.value();
-      double ang = atan2(y, x);
-      double mag = sqrt(pow(y, 2) + pow(x, 2));
-      double m1spd = mag*(2.0/3)*cos(ang + M_PI/3);
-      double m2spd = mag*(2.0/3)*cos(ang + 5*M_PI/3);
-      double m3spd = mag*(2.0/3)*cos(ang + M_PI);
+      // Calculate the angle between the x and y 
+      // directional vectors to determine the 
+      // direction we want the robot to move
+      double ang = atan2(y, x); 
+      // Calculate the magnitude that we want from the 
+      // robot's movement from the x and y directional vectors
+      double mag = sqrt(pow(y, 2) + pow(x, 2)); 
+      // Set Front Right Wheel (motor 1) speed using wheel equation
+      double m1spd = mag*(2.0/3)*cos(ang + M_PI/3); 
+      // Set Front Left Wheel (motor 2) speed using wheel equation
+      double m2spd = mag*(2.0/3)*cos(ang + 5*M_PI/3); 
+      // Set Back Wheel (motor 3) speed using wheel equation
+      double m3spd = mag*(2.0/3)*cos(ang + M_PI); 
+
+      // Print the X and Y coordinates of the controller's left joystick on the first line of the VEX Brain's screen
       Brain.Screen.setCursor(1, 1);
       Brain.Screen.print(x);
       Brain.Screen.newLine();
@@ -23,6 +37,8 @@ namespace godspeed
       Brain.Screen.newLine();
       task::sleep(100);
       Brain.Screen.clearScreen();
+
+      // Set the speed and direction of the motors so that when we turn them on, the robot moves in the desired direction (the direction the joystick is pointing)
       setMotorSpeed(m1spd, Motor1);
       setMotorSpeed(m2spd, Motor2);
       setMotorSpeed(m3spd, Motor3);
