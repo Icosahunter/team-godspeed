a7ce1eea8716cccb25f149907bd7026aa90def63
mark1659 <35717317+mark1659@users.noreply.github.com>
2020-12-14 16:22:21 -0600

    Create range-finder.h

---
+++ include/inputs/range-finder.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "framework/data-source.h"
+
+namespace godspeed
+{
+  namespace inputs
+  {
+    using namespace framework;
+
+    /**
+    * \brief A class containing data source objects corresponding to range finder inputs
+    */
+    class RangeFinder
+    {
+      public:
+        /// \brief A data source corresponding to the distance measured
+        static DataSource distance;
+
+      private:
+        static double distVal();
+
+        static void distSubscribe(void (*callback)(void));
+    };
+  }
+}
