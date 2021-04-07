a6de62fefc5fc15af6c57a7a10b4d984e96d9f50
Peter Moyer <petersdragon@gmail.com>
2021-04-06 19:36:55 -0500

    Added comments to bauble-states and state-machine

--- include/autonomous/bauble-states.h
+++ include/autonomous/bauble-states.h
@@ -11,96 +11,140 @@ namespace godspeed
 {
   namespace Bauble
   {
+    // Get into position once the match starts
     void ent1()
     {
-      // Get into position
-      DO_FOR(behaviors::MoveForward, 2700);
-      DO_FOR(behaviors::StopY, 500);
-      DO_FOR(behaviors::TurnRight, 2150);
-      DO_FOR(behaviors::StopAngle, 500);
+      // Move forward far enough to easily identify the backboard and align
+      DO_FOR(behaviors::MoveForward, 2700); // move forward (time in ms)
+      DO_FOR(behaviors::StopY, 500); // To flush out the averaging array
+      
+      // Turn right to face the tower in the home row right corner
+      DO_FOR(behaviors::TurnRight, 2150); // turn right (time in ms)
+      DO_FOR(behaviors::StopAngle, 500); // To flush out the averaging array
     }
 
+    // Approach goal in home row right corner, pickup the ball on the ground,
+    // and score the ball that was preloaded
     void ent2()
     {
+      // In case of past distance data, initialize new goal distance to infinity
       inputs::VisionSensor::GoalDistVar.Initialize(infinity());
-      // Approach goal
+      
+      // Start ball scorer and ball pickup 
       Binder::AddBinding(behaviors::PickUpBall);
       Binder::AddBinding(behaviors::ScoreBall);
       WAIT(1200);
-      // Finish scoring
+      
+      // Stop the robot in front of the tower to finish scoring
       DO_FOR(behaviors::StopY, 600);
-      // Finish picking up ball
+      
+      // Finish picking up ball and back away from the tower
       Binder::AddBinding(behaviors::PickUpBall);
       DO_FOR(behaviors::MoveBackward, 2500);
+      
+      // Stop the ball scorer and ball pickup
       Binder::RemoveBinding(behaviors::PickUpBall);
       Binder::RemoveBinding(behaviors::ScoreBall);
       outputs::BallCollector::TreadSpeed(0);
       outputs::BallScorer::TreadSpeed(0);
-      // Move to other goal
+      
+      // Stop backing up, turn to face the autonomous goal on the right edge
       DO_FOR(behaviors::StopY, 500);
       DO_FOR(behaviors::TurnLeft, 2000);
     }
 
+    // Approach and score the ball in the autonomous goal on the right edge,
+    // then remove the opponent's ball from the bottom of the tower
     void ent3()
     {
+      // In case of past distance data, initialize new goal distance to infinity
       inputs::VisionSensor::GoalDistVar.Initialize(infinity());
+      
       // Approach goal
       Binder::AddBinding(behaviors::ScoreBall);
       WAIT(1500);
-      // Finish scoring
+      
+      // Stop the robot in front of the tower to finish scoring
       DO_FOR(behaviors::StopY, 500);
       WAIT(2000);
+      
+      // Stop the ball scorer
       Binder::RemoveBinding(behaviors::ScoreBall);
       outputs::BallScorer::TreadSpeed(0);
+      
+      // Remove the ball from the tower
+      // Start picking up
       Binder::AddBinding(behaviors::PickUpBall);
+      
+      // Back up slightly
       DO_FOR(behaviors::MoveBackward, 500);
       DO_FOR(behaviors::StopY, 500);
+      
+      // Move forward slightly and turn to dislodge the ball
       Binder::AddBinding(behaviors::MoveForward);
       Binder::AddBinding(behaviors::TurnLeft);
       WAIT(500);
+      
+      // Move forward slightly to push the ball out or grab it
       Binder::RemoveBinding(behaviors::MoveForward);
       Binder::AddBinding(behaviors::StopY);
       WAIT(1500);
+
+      // Turn left to remove the ball from the tower
       Binder::RemoveBinding(behaviors::TurnLeft);
       Binder::RemoveBinding(behaviors::StopY);
+
+      // Stop the ball pickup
       Binder::RemoveBinding(behaviors::PickUpBall);
       outputs::BallCollector::TreadSpeed(0);
       outputs::OmniDrive3Wheel::AngleSpeed(0);
     }
 
+    // Define the ball scorer expander and how many degrees to rotate it
     double expander_pos() { return 355; }
     Binding ExpanderBinding(expander_pos, outputs::BallScorer::ExpanderPosition);
 
     void StartAutonomous()
     {
+      // Fine-tuning for Bauble to perform better (fudge numbers for the smaller robot)
       inputs::VisionSensor::XOffsetFudge = 0.1;
+
+      // Initialize the ball storage counter with the preloaded ball
       inputs::BallStorage::BallCounter = 1;
+
+      // Add the binding for the ball scorer expander to the list
       Binder::AddBinding(ExpanderBinding);
 
+      // Define state names
       static State s1;
       static State s2;
       static State s3;
       static State sStop;
 
+      // Define entry action, activities, exit conditions, and exit actions
       s1.AddEntryAction(ent1);
       s1.AddActivity(behaviors::AlignWithGoal);
       s1.AddActivity(behaviors::MoveForward);
       s1.AddTransition(conditions::NearGoal, s2);
 
+      // Define entry action, activities, exit conditions, and exit actions
       s2.AddEntryAction(ent2);
       s2.AddActivity(behaviors::AlignWithGoal);
       s2.AddActivity(behaviors::MoveForward);
       s2.AddTransition(conditions::NearGoal, s3);
 
+      // Define entry action, activities, exit conditions, and exit actions
       s3.AddEntryAction(ent3);
       s3.AddTransition(conditions::True, sStop);
 
+      // Define entry action, activities, exit conditions, and exit actions
       sStop.AddActivity(behaviors::StopX);
       sStop.AddActivity(behaviors::StopY);
       sStop.AddActivity(behaviors::StopAngle);
       sStop.AddActivity(behaviors::StopCollectors);
       sStop.AddActivity(behaviors::StopScorer);
 
+      // Start running the state machine
       StateMachine::Start(s1);
     }
   }
--- include/autonomous/state-machine.h
+++ include/autonomous/state-machine.h
@@ -14,26 +14,35 @@ namespace godspeed
   {
     public:
 
+    // Define lists to set up each state with its behaviors
+    // List to track what actions to execute when the state is entered
     std::list<ACTION> entryActions;
+    // List to track what actions to execute when the state is exited
     std::list<ACTION> exitActions;
+    // List to track the conditions needed to enter another state
     std::list<TRANSITION> transitions;
+    // List to track the activities that will be active when in the state
     std::list<Binding*> activities;
 
+    // Add a transition condition to the state's list
     void AddTransition(bool(*condition)(void), State &state)
     {
       transitions.push_back(TRANSITION(condition, &state));
     }
 
+    // Add an entry action to the state's list
     void AddEntryAction(void(*action)(void))
     {
       entryActions.push_back(action);
     }
 
+    // Add an exit action to the state's list
     void AddExitAction(void(*action)(void))
     {
       exitActions.push_back(action);
     }
 
+    // Add an activity to the state's list
     void AddActivity(Binding &activity)
     {
       activities.push_back(&activity);
@@ -42,25 +51,34 @@ namespace godspeed
 
   namespace StateMachine
   {
+    // Reference to the active state
     State* currentState;
 
+    // Handle the transition between states (execute exit actions, entry actions)
     void ChangeState(State &state)
     {
+      // Stop executing activities of the active state
       for(auto& b : currentState->activities)
       {
         Binder::RemoveBinding(*b);
       }
+
+      // Execute exit actions
       for(auto&a : currentState->exitActions)
       {
         a();
       }
       
+      // Set the next state as the active state
       currentState = &state;
 
+      // Execute entry actions
       for(auto& a : currentState->entryActions)
       {
         a();
       }
+
+      // Execute activities of the newly activated state
       for(auto& b : currentState->activities)
       {
         Binder::AddBinding(*b);
@@ -70,12 +88,19 @@ namespace godspeed
     /// \brief Update function called by the State Machine thread
     void Update()
     {
+      // Track whether the state has changed
       bool stateChanged = false;
+      // Reference to the current active state
       State* cur;
 
       while (true)
       {
+        // If the current active state changed since the last time
+        // through the loop, update it
         cur = currentState;
+        
+        // Check the conditions for each transition to see if any are met.
+        // If one is, make the transition
         for(auto const &t : cur->transitions)
         {
           if (std::get<0>(t)())
@@ -88,13 +113,19 @@ namespace godspeed
       }
     }
 
+    // Handle entering the first state
     void Start(State &starting_state)
     {
+      // Set the first state to the current active state
       currentState = &starting_state;
+
+      // Handle entry actions
       for(auto& a : currentState->entryActions)
       {
         a();
       }
+
+      // Start activities
       for(auto& b : currentState->activities)
       {
         Binder::AddBinding(*b);
