eb8d2fb95414060a7f8569e5ec40f7128405db6c
Peter Moyer <petersdragon@gmail.com>
2021-04-06 21:08:53 -0500

    Added comments to vision-sensor

--- include/inputs/vision-sensor.h
+++ include/inputs/vision-sensor.h
@@ -21,17 +21,22 @@ namespace godspeed
       /// \brief Horizontal field of view in radians
       double HorizontalFOV = 30.2145 * (3.14/180);
 
+      // Fine-tuning value to center the robot on the object instead of the camera
       double XOffsetFudge = 0;
 
+      // Define in inches the width and height of a ball 
       double BallWidth = 6.3;
       double BallHeight = 6.3;
 
+      // Define in inches the width and height of a goal's backboard
       double BackboardWidth = 4.375;
       double BackboardHeight = 6.1875;
 
+      // Return the minimum of the set of values read by the Vision Sensor
       WinMin BallDistVar(50);
       WinMin GoalDistVar(50);
 
+      // Return the average of the set of values read by the Vision Sensor
       WinAvg BallCountVar(10);
       WinAvg GoalCountVar(10);
 
@@ -42,27 +47,42 @@ namespace godspeed
       }
 
       /// \brief Calculated distance to an object, given it's dimensions etc.
+      // The camera will always think that an object is farther away than it
+      // actually is. However, the minimum of the ratio between the actual defined
+      // size and the measured is the most accurate distance
       double CalculateDistance(double obj_w, double obj_h, double obj_x, double obj_w_actual, double obj_h_actual)
       {
+        // Vertical component of the orthogonal distance to the object
         double orth_dist_v = ((obj_h_actual/obj_h)*ScreenHeight/2)/std::tan(VerticalFOV);
+        // Horizontal component of the orthogonal distance to the object
         double orth_dist_h = ((obj_w_actual/obj_w)*ScreenWidth/2)/std::tan(HorizontalFOV);
+        // Minimum calculated orthogonal distance to the object      
         double orth_dist = std::min(orth_dist_v, orth_dist_h);
+        // Determine whether the height or the width makes the object look closer
         double ratio = std::min(obj_h_actual/obj_h, obj_w_actual/obj_w);
+        // Determine the parallel component of the distance between the robot and the object
         double leg1 = ratio*obj_x + ratio*obj_w/2 - ratio*ScreenWidth/2;
+        // Determine the orthogonal component of the distance between the robot and the object
         double leg2 = orth_dist;
+        // Return the distance from the robot to the object
         return std::sqrt(std::pow(leg1, 2) + std::pow(leg2, 2));
       }
 
       /// \brief Calculated distance to the largest object given it's signature and actual width and height
       double GetDistance(signature sig, double obj_w, double obj_h)
       {
+        // Capture a frame from the Vision Sensor
         Vision20.takeSnapshot(sig, 1);
+        // Determine the largest object signature that matches the desired signature
         vision::object obj = Vision20.largestObject;
 
+        // If the Vision Sensor identifies a signature, return its values
         if (obj.originX != 0)
         {
           return CalculateDistance(obj.width, obj.height, obj.originX, obj_w, obj_h);
         }
+
+        // Otherwise return infinity
         else
         {
           return infinity();
